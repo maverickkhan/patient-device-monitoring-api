81eaa3c873e29861a18c451809cda07ef6b8a1a1
Abdul Hai <57320922+maverickkhan@users.noreply.github.com>
2025-04-11 23:48:53 +0500

    feat: device, device data modules done

---
+++ src/shared/services/jwt.strategy.ts
@@ -0,0 +1,42 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+  Logger,
+} from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JWT_SECRET, JwtPayload } from 'src/common';
+import { omit } from 'lodash';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  logger = new Logger(JwtStrategy.name);
+  constructor(
+    private readonly prisma: PrismaService,
+  ) {
+    super({
+      secretOrKey: JWT_SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+  async validate(payload: JwtPayload) {
+    this.logger.log(`validate payload: ${JSON.stringify(payload)}`);
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: {
+            id: payload.id,
+            deletedAt: null
+        },
+      });
+      if (!user) {
+        throw new UnauthorizedException('User not found')
+      }
+      return omit(user, ['password']);
+    } catch (error) {
+      this.logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
--- src/shared/services/services.module.ts
+++ src/shared/services/services.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { CrypterService } from './crypter/crypter.service';
+import { JwtStrategy } from './jwt.strategy';
+import { RepositoriesModule } from '../repositories/repositories.module';
+import { GqlAuthGuard } from './gql-authguard';
+import { ParserService } from './parser/parser.service';
 
 @Module({
-    providers: [PrismaService, CrypterService],
-    exports: [PrismaService, CrypterService]
+    // imports: [RepositoriesModule],
+    providers: [PrismaService, CrypterService, JwtStrategy, GqlAuthGuard, ParserService],
+    exports: [PrismaService, CrypterService, JwtStrategy, GqlAuthGuard, ParserService]
 })
 export class ServicesModule {}
